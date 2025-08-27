81b9223fd7b87ffdcedeacd9705e640b56d4baec
Sandra Delgado <sandragullit@gmail.com>
2025-08-27 17:19:48 -0600

    Initial tutorial for nextjs and client side routing

---
+++ weekThree/client-side-routing/src/App.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes,
+  Outlet,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route path="about" element={<About />}></Route>
+            <Route path="users" element={<Users />}></Route>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <Outlet />
+    </div>
+  );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+export default App;
