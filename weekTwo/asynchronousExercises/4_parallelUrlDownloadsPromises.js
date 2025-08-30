1db4cfe9f5da8bc730e97b1daed0fb96fba3c4b1
Sandra Delgado <sandragullit@gmail.com>
2025-08-30 11:57:03 -0600

    Handles parallel downloads with promises

---
+++ weekTwo/asynchronousExercises/4_parallelUrlDownloadsPromises.js
@@ -0,0 +1,36 @@
+"use strict";
+// Write a JavaScript function that takes an array of URLs and downloads
+//  the contents of each URL in parallel using Promises.
+
+function downloads(urls) {
+  const promises = urls.map((url) => {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("HTTP error! Status: ", response.status);
+          }
+          return response.text();
+        })
+        .then((data) => resolve(data))
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  });
+  return Promise.all(promises);
+}
+
+const urls = [
+  "https://jsonplaceholder.typicode.com/posts/1",
+  "https://jsonplaceholder.typicode.com/posts/2",
+  "https://jsonplaceholder.typicode.com/posts/3",
+];
+
+downloads(urls)
+  .then((contents) => {
+    console.log("Downloaded contents: ", contents);
+  })
+  .catch((error) => {
+    console.log("Error", error.message);
+  });
