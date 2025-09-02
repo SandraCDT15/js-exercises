29d337524f12da3e6990e65830ebd6f5ee7b9e9f
Sandra Delgado <sandragullit@gmail.com>
2025-09-02 13:52:28 -0600

    Add AppBar component

---
+++ weekFour/exercise-1-ssr/src/components/Appbar.jsx
@@ -0,0 +1,15 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+const Appbar = () => {
+  return (
+    <div>
+      <AppBar>
+        <Toolbar>
+          <Typography>Hello</Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Appbar;
