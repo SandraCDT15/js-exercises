5af34d3ac039d3d4a65939cf252270d707f343f6
Sandra Delgado <sandragullit@gmail.com>
2025-09-08 14:14:55 -0600

    Handles an error with try catch in an async function

--- weekTwo/promisesAsyncAwait/8_errorHandleAsyncAwait.js
+++ weekTwo/promisesAsyncAwait/8_errorHandleAsyncAwait.js
@@ -1,7 +1,31 @@
 "use strict";
 // Write a JavaScript program to demonstrate how to handle errors in async/await functions using try...catch
 
+function asyncTask(taskName, sec) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() >= 0.5) {
+        resolve(`${taskName} completed in ${sec} seconds`);
+      } else {
+        reject(`Error in ${taskName}`);
+      }
+    }, sec * 1000);
+  });
+}
+
 async function handleError() {
   try {
-  } catch {}
+    const task1 = await asyncTask("Task1", 2);
+    console.log(task1);
+
+    const task2 = await asyncTask("Task2", 3);
+    console.log(task2);
+
+    const task3 = await asyncTask("Task3", 5);
+    console.log(task3);
+  } catch (err) {
+    console.log(err);
+  }
 }
+
+handleError();
